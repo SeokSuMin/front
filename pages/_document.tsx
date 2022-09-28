cbc699272265f9e6b8eb2225b440e6c40d791b37
sumin <yahoo2344@naver.com>
2022-09-28 21:50:09 +0900

    _document https meta 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head></Head>
+                <Head>
+                    <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
